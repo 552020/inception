739abb53c7ff79857782768e3f83f995c889cb0f
552020 <stefanolombardo@posteo.de>
2024-09-13 23:08:25 +0200

    working on wordpress

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -6,6 +6,7 @@ MYSQL_USER_PASSWORD=$(cat /run/secrets/mysql_user_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
+export HTTP_HOST="$DOMAIN_NAME"
 
 # Check if the WordPress directory exists
 if [ -d /var/www/wordpress ]; then
@@ -23,22 +24,14 @@ fi
 
 # Check if wp-config.php already exists and environment variables are set
 if [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_USER_PASSWORD" ] || [ -z "$DB_HOST" ]; then
-
     echo "Database credentials not set!"
     exit 1
 fi
 
-# Print out database connection details for debugging
-echo "Attempting to connect to MariaDB:"
-echo "DB_HOST: $DB_HOST"
-echo "MYSQL_DATABASE: $MYSQL_DATABASE"
-echo "MYSQL_USER: $MYSQL_USER"
-echo "MYSQL_USER_PASSWORD: $MYSQL_USER_PASSWORD"
-
 # Test the connection to MariaDB before proceeding
 timeout=180
 waited=0
-until mysqladmin ping --host=$DB_HOST --user=$MYSQL_USER --password=$MYSQL_USER_PASSWORD --silent; do
+until mysqladmin ping --host=$DB_HOST --user=$MYSQL_USER --password=$MYSQL_USER_PASSWORD; do
     echo "Waiting for MariaDB connection... waited ${waited} seconds."
     sleep 5
     waited=$((waited+5))
@@ -60,35 +53,37 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
                      || { echo "wp-config.php creation failed!"; exit 1; }
 fi
 
-# Perform WordPress core installation
+# Check if WordPress is already installed
 if ! wp core is-installed --allow-root; then
-	echo "WordPress core not installed!"
-	exit 1
+    echo "WordPress core is not installed. Proceeding with installation..."
+    
+    # Perform WordPress core installation
+    wp core install --url="$DOMAIN_NAME" \
+                    --title="$SITE_TITLE" \
+                    --admin_user="$WP_ADMIN_NAME" \
+                    --admin_password="$WP_ADMIN_PASSWORD" \
+                    --admin_email="$WP_ADMIN_EMAIL" \
+                    --allow-root || { echo "WordPress installation failed!"; exit 1; }
+else
+    echo "WordPress core is already installed. Skipping installation."
 fi
 
-wp core install --url="$DOMAIN_NAME" \
-				--title="$SITE_TITLE" \
-				--admin_user="$WP_ADMIN" \
-				--admin_password="$WP_ADMIN_PASSWORD" \
-				--admin_email="$WP_ADMIN_EMAIL" \
-				--allow-root
-
 # Create a new WordPress user
-if [ -z "$WP_USER" ] || [ -z "$WP_USER_EMAIL" ] || [ -z "$WP_USER_PASSWORD" ]; then
+if [ -z "$WP_USER_NAME" ] || [ -z "$WP_USER_EMAIL" ] || [ -z "$WP_USER_PASSWORD" ]; then
     echo "User details not set!"
     exit 1
 fi
 
-if ! wp user get $WP_USER --allow-root > /dev/null 2>&1; then
-    wp user create --allow-root $WP_USER $WP_USER_EMAIL --role=editor --user_pass=$WP_USER_PASSWORD
+if ! wp user get $WP_USER_NAME --allow-root > /dev/null 2>&1; then
+    wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL --role=editor --user_pass=$WP_USER_PASSWORD
 else
-    echo "User $WP_USER already exists."
+    echo "User $WP_USER_NAME already exists."
 fi
 
-# Ensure PHP-FPM directory exists
-if [ ! -d /run/php ]; then
-    mkdir /run/php
-fi
+# # Ensure PHP-FPM directory exists
+# if [ ! -d /run/php ]; then
+#     mkdir /run/php
+# fi
 
-# Run PHP-FPM
-php-fpm81 -F
+# # Run PHP-FPM
+# php-fpm81 -F
